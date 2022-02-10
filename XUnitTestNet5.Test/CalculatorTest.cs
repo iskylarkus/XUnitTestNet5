ce3f72b1aa48b15e3177aa0ec536e0cd4718b979
iskylarkus <turgay.kibar@gmail.com>
2022-02-10 18:14:30 +0300

    xunit test developed

---
+++ XUnitTestNet5.Test/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using XUnitTestNet5.App;
+
+namespace XUnitTestNet5.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.Add(a, b);
+
+            //Assert
+            Assert.Equal<int>(25, total);
+        }
+    }
+}
