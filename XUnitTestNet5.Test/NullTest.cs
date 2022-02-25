2f2057f564c844cd6dc42bad530120604233329a
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 10:26:16 +0300

    null test developed

---
+++ XUnitTestNet5.Test/NullTest.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class NullTest
+    {
+        [Fact]
+        public void NullPassedTest()
+        {
+            string name = null;
+
+            Assert.Null(name);
+        }
+
+        [Fact]
+        public void NullFailedTest()
+        {
+            string name = "ilker";
+
+            Assert.Null(name);
+        }
+
+        [Fact]
+        public void NotNullPassedTest()
+        {
+            string name = "ilker";
+
+            Assert.NotNull(name);
+        }
+
+        [Fact]
+        public void NotNullFailedTest()
+        {
+            string name = null;
+
+            Assert.NotNull(name);
+        }
+    }
+}
