0c03a797bc17260216f66bcf65a26ca6acacd060
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 10:12:43 +0300

    istype test developed

---
+++ XUnitTestNet5.Test/IsTypeTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class IsTypeTest
+    {
+        [Fact]
+        public void IsTypePassedTest()
+        {
+            Assert.IsType<string>("ilker");
+        }
+
+        [Fact]
+        public void IsTypeFailedTest()
+        {
+            Assert.IsType<int>("ilker");
+        }
+
+        [Fact]
+        public void IsNotTypePassedTest()
+        {
+            Assert.IsNotType<int>("ilker");
+        }
+
+        [Fact]
+        public void IsNotTypeFailedTest()
+        {
+            Assert.IsNotType<string>("ilker");
+        }
+    }
+}
