a195e3608e0e52c067187044c53ce4f9007b1e20
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 10:06:19 +0300

    single test project created

---
+++ XUnitTestNet5.Test/SingleTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class SingleTest
+    {
+        [Fact]
+        public void SinglePassedTest()
+        {
+            Assert.Single(new List<string>() { "ilker"});
+        }
+
+        [Fact]
+        public void SingleFailedTest()
+        {
+            Assert.Single(new List<string>() { "ilker", "selvi" });
+        }
+
+        [Fact]
+        public void GenericSinglePassedTest()
+        {
+            Assert.Single<int>(new List<int>() { 19 });
+        }
+
+        [Fact]
+        public void GenericSingleFailedTest()
+        {
+            Assert.Single<int>(new List<int>() { 19, 11 });
+        }
+    }
+}
