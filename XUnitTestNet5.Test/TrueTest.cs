2e747564a1da945f7f05ea4a50d6535de4d3635e
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 08:55:03 +0300

    true test project created

---
+++ XUnitTestNet5.Test/TrueTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class TrueTest
+    {
+        [Fact]
+        public void TruePassedTest()
+        {
+            Assert.True(5 > 3);
+        }
+
+        [Fact]
+        public void TrueFailedTest()
+        {
+            Assert.True(5 < 3);
+        }
+
+        [Fact]
+        public void FalsePassedTest()
+        {
+            Assert.False("".GetType() == typeof(int));
+        }
+
+        [Fact]
+        public void FalseFailedTest()
+        {
+            Assert.False("".GetType() == typeof(string));
+        }
+    }
+}
