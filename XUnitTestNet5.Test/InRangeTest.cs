98f8332daf72d09c450b03f6abb945d17142a01b
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 10:02:09 +0300

    inrange test project created

---
+++ XUnitTestNet5.Test/InRangeTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class InRangeTest
+    {
+        [Fact]
+        public void InRangePassedTest()
+        {
+            Assert.InRange(10, 2, 20);
+        }
+
+        [Fact]
+        public void InRangeFailedTest()
+        {
+            Assert.InRange(25, 2, 20);
+        }
+
+        [Fact]
+        public void NotInRangePassedTest()
+        {
+            Assert.NotInRange(25, 2, 20);
+        }
+
+        [Fact]
+        public void NotInRangeFailedTest()
+        {
+            Assert.NotInRange(10, 2, 20);
+        }
+    }
+}
