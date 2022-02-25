104f4ae02f048153e6d803e54bda4da14e1a5e6a
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 10:50:00 +0300

    add test with inline data of params developed

--- XUnitTestNet5.Test/CalculatorTest.cs
+++ XUnitTestNet5.Test/CalculatorTest.cs
@@ -19,5 +19,18 @@ namespace XUnitTestNet5.Test
             //Assert
             Assert.NotEqual<int>(29, total);
         }
+
+        [Theory]
+        [InlineData(1, 6, 7)]
+        [InlineData(19, 11, 33)]
+        [InlineData(35, 42, 77)]
+        public void AddTestParam(int a, int b, int expectedTotal)
+        {
+            var calculator = new Calculator();
+
+            var actualTotal = calculator.Add(a, b);
+
+            Assert.Equal(expectedTotal, actualTotal);
+        }
     }
 }
