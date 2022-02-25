5171c15d30724db8938e0dae9f31d56a8ea940b6
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 08:44:45 +0300

    contain test project created

---
+++ XUnitTestNet5.Test/ContainTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class ContainTest
+    {
+        [Fact]
+        public void RunContainPassedTest()
+        {
+            //Assert
+            Assert.Contains("ilker", "ilker selvi");
+        }
+
+        [Fact]
+        public void RunContainFailedTest()
+        {
+            //Assert
+            Assert.Contains("fatih", "ilker selvi");
+        }
+
+        [Fact]
+        public void RunDoesNotContainPassedTest()
+        {
+            var names = new List<string>() { "fatih", "ilker", "selvi" };
+
+            Assert.DoesNotContain(names, x => x == "emin");
+        }
+
+        [Fact]
+        public void RunDoesNotContainFailedTest()
+        {
+            var names = new List<string>() { "fatih", "ilker", "selvi" };
+
+            Assert.DoesNotContain(names, x => x == "ilker");
+        }
+    }
+}
