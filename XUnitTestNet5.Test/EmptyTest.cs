1197965c0d7113f11c6c39be8008bdedd54ac202
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 09:58:13 +0300

    empty test project created

---
+++ XUnitTestNet5.Test/EmptyTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class EmptyTest
+    {
+        [Fact]
+        public void EmptyPassedTest()
+        {
+            Assert.Empty(new List<string>());
+        }
+
+        [Fact]
+        public void EmptyFailedTest()
+        {
+            Assert.Empty(new List<string>() { "ilker", "selvi" });
+        }
+
+        [Fact]
+        public void NotEmptyPassedTest()
+        {
+            Assert.NotEmpty(new List<string>() { "ilker", "selvi" });
+        }
+
+        [Fact]
+        public void NotEmptyFailedTest()
+        {
+            Assert.NotEmpty(new List<string>());
+        }
+    }
+}
