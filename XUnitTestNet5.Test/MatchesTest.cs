902b8b7843c947ef6b18ce28a668fc48d6fa28be
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 09:06:06 +0300

    matches test project created

---
+++ XUnitTestNet5.Test/MatchesTest.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class MatchesTest
+    {
+        [Fact]
+        public void MatchPassedTest()
+        {
+            var regex = "^ilker";
+
+            Assert.Matches(regex, "ilker selvi");
+        }
+
+        [Fact]
+        public void MatchesFailedTest()
+        {
+            var regex = "^ilker";
+
+            Assert.Matches(regex, "selvi ilker");
+        }
+
+        [Fact]
+        public void DoesNotMatchPassedTest()
+        {
+            var regex = "^ilker";
+
+            Assert.DoesNotMatch(regex, "selvi ilker");
+        }
+
+        [Fact]
+        public void DoesNotMatchFailedTest()
+        {
+            var regex = "^ilker";
+
+            Assert.DoesNotMatch(regex, "ilker selvi");
+        }
+    }
+}
