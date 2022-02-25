26a48bf7a2cc3e24f5da340f3e76a4ea85ec07fd
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 10:22:02 +0300

    isAssignableFrom test developed

---
+++ XUnitTestNet5.Test/IsAssignableFromTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class IsAssignableFromTest
+    {
+        [Fact]
+        public void IsAssignableFromPassedTest()
+        {
+            Assert.IsAssignableFrom<object>("ilker");
+        }
+
+        [Fact]
+        public void IsAssignableFromFailedTest()
+        {
+            Assert.IsAssignableFrom<int>(new object());
+        }
+
+        [Fact]
+        public void GenericIsAssignableFromPassedTest()
+        {
+            Assert.IsAssignableFrom<IEnumerable<string>>(new List<string>());
+        }
+
+        [Fact]
+        public void GenericIsAssignableFromFailedTest()
+        {
+            Assert.IsAssignableFrom<IEnumerable<string>>(new List<int>());
+        }
+    }
+}
