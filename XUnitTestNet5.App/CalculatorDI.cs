2bc277e686d2aa09e40a79fa61e55686b327a9e6
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 13:51:46 +0300

    calculator class using dependency injection service interface developed

---
+++ XUnitTestNet5.App/CalculatorDI.cs
@@ -0,0 +1,22 @@
+﻿namespace XUnitTestNet5.App
+{
+    public class CalculatorDI
+    {
+        private readonly ICalculatorService _calculatorService;
+
+        public CalculatorDI(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        public int Add(int a, int b)
+        {
+            return _calculatorService.Add(a, b);
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return _calculatorService.Multiply(a, b);
+        }
+    }
+}
