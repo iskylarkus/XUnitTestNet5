66e851edd96aa2f65ed4295690fdbfc783f6db04
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 08:38:22 +0300

    equal test project created

---
+++ XUnitTestNet5.Test/EqualTest.cs
@@ -0,0 +1,68 @@
+﻿using Xunit;
+using XUnitTestNet5.App;
+
+namespace XUnitTestNet5.Test
+{
+    public class EqualTest
+    {
+        [Fact]
+        public void RunEqualPassedTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.Add(a, b);
+
+            //Assert
+            Assert.Equal<int>(25, total);
+        }
+
+        [Fact]
+        public void RunEqualFailedTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.Add(a, b);
+
+            //Assert
+            Assert.Equal<int>(29, total);
+        }
+
+        [Fact]
+        public void RunNotEqualPassedTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.Add(a, b);
+
+            //Assert
+            Assert.NotEqual<int>(29, total);
+        }
+
+        [Fact]
+        public void RunNotEqualFailedTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.Add(a, b);
+
+            //Assert
+            Assert.NotEqual<int>(25, total);
+        }
+    }
+}
