93f5a8e84af4c5f765867233df2161b33824cef4
iskylarkus <turgay.kibar@gmail.com>
2022-02-25 09:53:35 +0300

    startswith test project created

---
+++ XUnitTestNet5.Test/StartsWithTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace XUnitTestNet5.Test
+{
+    public class StartsWithTest
+    {
+        [Fact]
+        public void StartsWithPassedTest()
+        {
+            Assert.StartsWith("ilker", "ilker selvi");
+        }
+
+        [Fact]
+        public void StartsWithFailedTest()
+        {
+            Assert.StartsWith("selvi", "ilker selvi");
+        }
+
+        [Fact]
+        public void EndsWithPassedTest()
+        {
+            Assert.EndsWith("selvi", "ilker selvi");
+        }
+
+        [Fact]
+        public void EndsWithFailedTest()
+        {
+            Assert.EndsWith("ilker", "ilker selvi");
+        }
+    }
+}
