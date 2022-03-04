7a83da1d820cefda91d563fed589dae5340d716b
iskylarkus <turgay.kibar@gmail.com>
2022-03-04 16:47:11 +0300

    web project created and then database created and then scaffold-dbcontext runned on pm for dbfirst

---
+++ XUnitTestNet5.Web/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+
+namespace XUnitTestNet5.Web.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public int? Stock { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ XUnitTestNet5.Web/Models/XUnitTestNet5Context.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace XUnitTestNet5.Web.Models
+{
+    public partial class XUnitTestNet5Context : DbContext
+    {
+        public XUnitTestNet5Context()
+        {
+        }
+
+        public XUnitTestNet5Context(DbContextOptions<XUnitTestNet5Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=XUnitTestNet5;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Color).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
