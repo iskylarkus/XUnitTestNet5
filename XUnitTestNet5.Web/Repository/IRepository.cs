ef47fbe72ec39b5293d127847978c240cabc983f
iskylarkus <turgay.kibar@gmail.com>
2022-03-04 17:11:31 +0300

    irepository interface implemented

---
+++ XUnitTestNet5.Web/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XUnitTestNet5.Web.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(int id);
+        Task Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
