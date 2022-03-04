7cca81139ffa8ad636d7f8516be5fecf5829f2b4
iskylarkus <turgay.kibar@gmail.com>
2022-03-04 17:24:22 +0300

    repository class with inheriting irepository interface developed

---
+++ XUnitTestNet5.Web/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using XUnitTestNet5.Web.Models;
+
+namespace XUnitTestNet5.Web.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly XUnitTestNet5Context _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(XUnitTestNet5Context context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
