5009deb7e71e96da5ccda0dd703a1514d9f03ddd
iskylarkus <turgay.kibar@gmail.com>
2022-03-15 16:34:47 +0300

    productscontroller test class created

---
+++ XUnitTestNet5.Web.Test/ProductsControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using System.Collections.Generic;
+using XUnitTestNet5.Web.Controllers;
+using XUnitTestNet5.Web.Models;
+using XUnitTestNet5.Web.Repository;
+
+namespace XUnitTestNet5.Web.Test
+{
+    public class ProductsControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepository;
+        private readonly ProductsController _productsController;
+        private List<Product> _products;
+
+        public ProductsControllerTest()
+        {
+            _mockRepository = new Mock<IRepository<Product>>();
+            _productsController = new ProductsController(_mockRepository.Object);
+            _products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Kalem", Color = "Kırmızı", Price = 11, Stock = 1111},
+                new Product() { Id = 2, Name = "Silgi", Color = "Sarı", Price = 19, Stock = 1919},
+            };
+        }
+    }
+}
