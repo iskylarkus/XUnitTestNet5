f03d498375fdb34cfbf9bfc2ec80810f153db5a1
iskylarkus <turgay.kibar@gmail.com>
2022-04-10 16:40:50 +0300

    product api conroller test created

---
+++ XUnitTestNet5.Web.Test/ProductApiControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using System.Collections.Generic;
+using XUnitTestNet5.Web.Controllers;
+using XUnitTestNet5.Web.Models;
+using XUnitTestNet5.Web.Repository;
+
+namespace XUnitTestNet5.Web.Test
+{
+    public class ProductApiControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mock;
+        private readonly ProductsApiController _controller;
+        private List<Product> _products;
+
+        public ProductApiControllerTest()
+        {
+            _mock = new Mock<IRepository<Product>>();
+            _controller = new ProductsApiController(_mock.Object);
+            _products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Kalem", Color = "Kırmızı", Price = 11, Stock = 1111},
+                new Product() { Id = 2, Name = "Silgi", Color = "Sarı", Price = 19, Stock = 1919},
+            };
+        }
+    }
+}
